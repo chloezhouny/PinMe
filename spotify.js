08814dfbf9582bb299ac36f621953d696485d70e
Chloe Zhou <chloe@Chloes-MacBook-Pro-2.local>
2019-06-10 17:04:50 -0700

    Add get buttons, for user to click on

--- spotify.js
+++ spotify.js
@@ -1,3 +1,51 @@
+var topics = ["1", "2", "3", "4", "5", "6", "7", 
+"8",  "9", "10", "11"];
+var topicsSearch = ["1", "2", "3", "4", "5", "6", "7", "8", "9",
+ "10", "11"];
+var userAdd = false;
+var start = true;
+var token;
+
+
+
+
+function getButton()
+{
+
+	if(start === true)
+	{
+		start = false;
+	}
+
+	if (userAdd === false)
+	{
+		for (var i = 0; i < topicsSearch.length; i++)
+		{
+			var btn = $("<button>").text(topics[i]);
+			btn.addClass("btn btn-dark options");
+			btn.attr("data-fitness", topicsSearch[i]);
+			btn.attr("type", "button");
+			$("#buttons-container").append(btn);
+		}
+		userAdd = true;
+	}
+	else
+	{		
+			var i = topicsSearch.length - 1 ;
+
+			var btn = $("<button>").text(topics[i]);
+			btn.addClass("btn btn-dark options");
+			btn.attr("data-fitness", topicsSearch[i]);
+			btn.attr("type", "button");
+			$("#buttons-container").append(btn);
+		
+	}
+}
+
+
+
+
+
 function getSpotifyToken()
 {
 	var tokenURL = "https://accounts.spotify.com/api/token";
@@ -31,4 +79,66 @@ function getSpotifyToken()
 
 
 }
+
+
+getButton();
+getSpotifyToken();
+
+$(document).on("click", ".options", function()
+{
+
+	$("#playlistDiv").text("");
+	var topicSearch = $(this).attr("data-fitness");
+	var state = $(this).attr("data-state");
+	var playlistURL = "https://api.spotify.com/v1/search?q=" + topicSearch + "&type=playlist&limit=10";	
+
+
+
+      /* Spotify playlist API */
+		$.ajax({
+			url: playlistURL,
+			method: "GET",
+			Accept: "application/json",
+			ContentType: "application/json",
+			headers: {
+			"Authorization": "Bearer "+ token}
+
+		})
+		.then(function(response){
+			console.log(response);
+			for(var i = 0; i<4; i++)
+		{
+
+			var result = response.playlists;
+			var playlistURL = result.items[i].external_urls.spotify;
+
+			var imgURL = result.items[i].images[0].url;
+		
+
+		
+
+			var playlists = $("<div id = 'playlist'>");
+			var playlist = $("<a href='" + playlistURL + "' target = 'blank'>");
+			var img = $("<img>");
+			img.attr("src", imgURL);
+			img.addClass("uk-animation-scale-up uk-transform-origin-top-left uk-transition-fade");
+			img.attr("background-color", "black")
+
+			var playDiv =  $("<div id='play'>").text("►");
+			playDiv.addClass("uk-transition-fade");
+			
+
+			playlist.addClass("uk-transition-toggle");
+			playlist.addClass("uk-overflow-hidden");
+			playlist.append(img);
+			playlist.append(playDiv);
+
+
+			playlists.append(playlist);
+			
+			$("#playlistDiv").append(playlists);
+		}
+	})
+
+});
 
