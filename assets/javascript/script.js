3037d4a439efd01f5830ac30b7a36fbfd7e11545
b0bland <alfredychan96@gmail.com>
2019-06-12 17:35:34 -0700

    Merged a few things

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,8 +2,6 @@ var locations = [];
 
 var labels = [];
 
-var gate = false;
-
 var userlong;
 var userlat;
 window.onload = function() {
@@ -106,13 +104,9 @@ function displayEvent() {
     // is this google map api?
     for (n = 0; n < 10; n++) {
       var eventloc = [];
-      eventloc.push(response.events[n].venue.latitude);
-      eventloc.push(response.events[n].venue.longitude);
+      eventloc.push(events[n].venue.latitude);
+      eventloc.push(events[n].venue.longitude);
       locations.push(eventloc);
-      if (n === 9) {
-        gate = true;
-        console.log("yes");
-      }
     }
     console.log(locations);
   });
@@ -153,3 +147,92 @@ $(".main-carousel").flickity({
 //
 
 getLocation();
+
+
+
+//Google Maps script
+
+setTimeout(initMap(),1000);
+function initMap() {
+
+  var myLatlng = new google.maps.LatLng(userlat, userlong)
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: myLatlng
+  });
+  
+  var bounds = new google.maps.LatLngBounds();
+  
+  $("#event-genre").on("click", function(event) {
+    event.preventDefault();
+      console.log(locations.length)
+      var marker;
+
+ 
+// loop through locations and add to map
+for ( var i = 0; i < locations.length; i++ )
+{
+  // get current location
+  var location = locations[ i ];
+  
+  // create map position
+  var position = new google.maps.LatLng( location[ 0 ], location[ 1 ] );
+  
+  // add position to bounds
+  bounds.extend( position );
+  
+  // create marker (https://developers.google.com/maps/documentation/javascript/reference#MarkerOptions)
+  marker = new google.maps.Marker({
+    /*animation: google.maps.Animation.DROP */
+      map: map
+    , position: position
+    , title: location[ 0 ]
+  });
+  
+  // create info window and add to marker (https://developers.google.com/maps/documentation/javascript/reference#InfoWindowOptions)
+  google.maps.event.addListener( marker, 'click', ( 
+    function( marker, i ) {
+      return function() {
+        var infowindow = new google.maps.InfoWindow();
+        infowindow.setContent( labels[i]);
+        infowindow.open( map, marker );
+      }
+    }
+  )( marker, i ) );
+      };
+    
+  })
+
+
+var infoWindow = new google.maps.InfoWindow;
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser does not support geolocation.');
+  infoWindow.open(map);
+}
+
+}
+
