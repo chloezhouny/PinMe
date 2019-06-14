1aa7b9dfef11217c0df3e74fcc9c525d6a663877
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-06-13 18:40:04 -0700

    added google auth

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,12 +19,11 @@ function getLocation() {
 
 var geogate = true;
 function error(err) {
-console.log(err.code)
-if (err.code === 1) {
-  geogate = false;
+  console.log(err.code);
+  if (err.code === 1) {
+    geogate = false;
+  }
 }
-}
-
 
 function showPosition(position) {
   console.log(
@@ -47,17 +46,19 @@ function getURL() {
   var date = $("#date").val();
   var queryURL;
   var basicURL;
-  
+
   if (geogate) {
-    basicURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&categories=103&token=HCI6R2VNZXBSOAT5UBT2&expand=venue&location.latitude=" +
-    userlat +
-    "&location.longitude=" +
-    userlong +
-    "&location.within=100mi";}
-  else if (!geogate) {
-    basicURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&categories=103&token=HCI6R2VNZXBSOAT5UBT2&expand=venue"
+    basicURL =
+      "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&categories=103&token=HCI6R2VNZXBSOAT5UBT2&expand=venue&location.latitude=" +
+      userlat +
+      "&location.longitude=" +
+      userlong +
+      "&location.within=100mi";
+  } else if (!geogate) {
+    basicURL =
+      "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&categories=103&token=HCI6R2VNZXBSOAT5UBT2&expand=venue";
   }
- 
+
   queryURL = basicURL;
 
   if (price !== "Any price") {
@@ -98,9 +99,17 @@ function displayEvent() {
       var eventName = $("<h3>").text(events[i].name.text);
       var eventSum = $("<p>").text(events[i].summary);
       var venueName = $("<p>").html("<b>Venue:</b> " + events[i].venue.name);
-      var eventTimeStart = events[i].start.local.split("T")
-      var eventStart = $("<p>").html("<b>Date</b>: " + eventTimeStart[0] + "<br>" + "<b>Time: </b>" + eventTimeStart[1]);
-      var eventPlace = $("<p>").html("<b>Address:</b> " + events[i].venue.address.localized_address_display);
+      var eventTimeStart = events[i].start.local.split("T");
+      var eventStart = $("<p>").html(
+        "<b>Date</b>: " +
+          eventTimeStart[0] +
+          "<br>" +
+          "<b>Time: </b>" +
+          eventTimeStart[1]
+      );
+      var eventPlace = $("<p>").html(
+        "<b>Address:</b> " + events[i].venue.address.localized_address_display
+      );
       var eventURL = $("<a />", {
         href: events[i].url,
         text: "read more"
@@ -108,10 +117,9 @@ function displayEvent() {
 
       var eventFree;
       if (events[i].is_free) {
-        eventFree = $("<p>").html("<b>Pricing:</b> Free")
-      }
-      else if (!events[i].is_free) {
-        eventFree = $("<p>").html("<b>Pricing:</b> Paid")
+        eventFree = $("<p>").html("<b>Pricing:</b> Free");
+      } else if (!events[i].is_free) {
+        eventFree = $("<p>").html("<b>Pricing:</b> Paid");
       }
 
       newEvent.append(eventName);
@@ -130,7 +138,12 @@ function displayEvent() {
       eventloc.push(events[i].venue.latitude);
       eventloc.push(events[i].venue.longitude);
       locations.push(eventloc);
-      labels.push("<div><b>Event:</b> " + response.events[i].name.html + "</div><div><b>Venue:</b> " + response.events[i].venue.name)
+      labels.push(
+        "<div><b>Event:</b> " +
+          response.events[i].name.html +
+          "</div><div><b>Venue:</b> " +
+          response.events[i].venue.name
+      );
     }
     console.log(locations);
   });
@@ -156,200 +169,215 @@ getLocation();
 
 var markers = [];
 
-setTimeout(initMap(),1000);
+setTimeout(initMap(), 1000);
 function initMap() {
+  var myLatlng = new google.maps.LatLng(67.880605, 12.982618);
 
-  var myLatlng = new google.maps.LatLng(67.880605, 12.982618)
-
-  
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: myLatlng
   });
-  
+
   var bounds = new google.maps.LatLngBounds();
 
   $("#event-genre").on("click", function(event) {
     event.preventDefault();
-    clearMarkers()
-      console.log(locations.length)
-      var marker;
+    clearMarkers();
+    console.log(locations.length);
+    var marker;
 
-setTimeout(function() { 
-// loop through locations and add to map
-for ( var i = 0; i < locations.length; i++ )
-{
-  // get current location
-  var location = locations[ i ];
-  
-  // create map position
-  var position = new google.maps.LatLng( location[ 0 ], location[ 1 ] );
-  
-  // add position to bounds
-  bounds.extend( position );
-  
-  // create marker (https://developers.google.com/maps/documentation/javascript/reference#MarkerOptions)
-  marker = new google.maps.Marker({
-    /*animation: google.maps.Animation.DROP */
-      map: map
-    , position: position
-    , title: location[ 0 ]
+    setTimeout(function() {
+      // loop through locations and add to map
+      for (var i = 0; i < locations.length; i++) {
+        // get current location
+        var location = locations[i];
+
+        // create map position
+        var position = new google.maps.LatLng(location[0], location[1]);
+
+        // add position to bounds
+        bounds.extend(position);
+
+        // create marker (https://developers.google.com/maps/documentation/javascript/reference#MarkerOptions)
+        marker = new google.maps.Marker({
+          /*animation: google.maps.Animation.DROP */
+          map: map,
+          position: position,
+          title: location[0]
+        });
+
+        markers.push(marker);
+
+        // create info window and add to marker (https://developers.google.com/maps/documentation/javascript/reference#InfoWindowOptions)
+        google.maps.event.addListener(
+          marker,
+          "click",
+          (function(marker, i) {
+            return function() {
+              var infowindow = new google.maps.InfoWindow();
+              infowindow.setContent(labels[i]);
+              infowindow.open(map, marker);
+            };
+          })(marker, i)
+        );
+      }
+    }, 3000);
   });
 
-  markers.push(marker);
-  
-  // create info window and add to marker (https://developers.google.com/maps/documentation/javascript/reference#InfoWindowOptions)
-  google.maps.event.addListener( marker, 'click', ( 
-    function( marker, i ) {
-      return function() {
-        var infowindow = new google.maps.InfoWindow();
-        infowindow.setContent( labels[i]);
-        infowindow.open( map, marker );
+  var infoWindow = new google.maps.InfoWindow();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("You are here.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      function() {
+        handleLocationError(true, infoWindow, map.getCenter());
       }
-    }
-  )( marker, i ) );
-      };
-    
-  },3000)
-}) 
-
-var infoWindow = new google.maps.InfoWindow;
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('You are here.');
-      infoWindow.open(map);
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
+    );
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser does not support geolocation.');
-  infoWindow.open(map);
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser does not support geolocation."
+    );
+    infoWindow.open(map);
+  }
 }
 
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
 }
 
-   // Sets the map on all markers in the array.
-   function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-    }
-  }
+// Removes the markers from the map.
+function clearMarkers() {
+  setMapOnAll(null);
+  locations = [];
+  labels = [];
+}
 
-  // Removes the markers from the map.
-  function clearMarkers() {
-    setMapOnAll(null);
-    locations = [];
-    labels = [];
-  }
-
-
-function getSpotifyToken()
-{
+function getSpotifyToken() {
   var tokenURL = "https://accounts.spotify.com/api/token";
-  var clientId = '5e15085d2b924d049ae29907ee452bbf';
-  var clientSecret = 'e84f853c2b784addb982d679f609d73a';
-  var encodedData = window.btoa(clientId + ':' + clientSecret);
+  var clientId = "5e15085d2b924d049ae29907ee452bbf";
+  var clientSecret = "e84f853c2b784addb982d679f609d73a";
+  var encodedData = window.btoa(clientId + ":" + clientSecret);
 
   console.log("HI");
-    jQuery.ajaxPrefilter(function(options) {
-      if (options.crossDomain && jQuery.support.cors) {
-          options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-      }
+  jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+      options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
+    }
   });
 
-
-    $.ajax({
-        method: "POST",
-        url: tokenURL,
-        data: {
-          grant_type: 'client_credentials'
-        },
-      headers: {
-        "Authorization": "Basic "+ encodedData,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'x-requested-with': 'XMLHttpRequest'
-      }
-    })
-        .then (function(result) {
-          console.log(result);
-          token1 = result.access_token;
-     });
-
-
+  $.ajax({
+    method: "POST",
+    url: tokenURL,
+    data: {
+      grant_type: "client_credentials"
+    },
+    headers: {
+      Authorization: "Basic " + encodedData,
+      "Content-Type": "application/x-www-form-urlencoded",
+      "x-requested-with": "XMLHttpRequest"
+    }
+  }).then(function(result) {
+    console.log(result);
+    token1 = result.access_token;
+  });
 }
-
 
 getSpotifyToken();
 
- function playList()
- {
+function playList() {
   $("#playlistDiv").text("");
   console.log(token1);
-  
+
   var topicSearch = $(this).attr("data-fitness");
   var state = $(this).attr("data-state");
-  var playlistURL = "https://api.spotify.com/v1/search?q=" + genreText + "&type=playlist&limit=10"; 
+  var playlistURL =
+    "https://api.spotify.com/v1/search?q=" +
+    genreText +
+    "&type=playlist&limit=10";
 
-
-
-      /* Spotify playlist API */
-    $.ajax({
-      url: playlistURL,
-      method: "GET",
-      Accept: "application/json",
-      ContentType: "application/json",
-      headers: {
-      "Authorization": "Bearer "+ token1}
-
-    })
-    .then(function(response){
-      console.log(response);
-      for(var i = 0; i<4; i++)
-    {
-
+  /* Spotify playlist API */
+  $.ajax({
+    url: playlistURL,
+    method: "GET",
+    Accept: "application/json",
+    ContentType: "application/json",
+    headers: {
+      Authorization: "Bearer " + token1
+    }
+  }).then(function(response) {
+    console.log(response);
+    for (var i = 0; i < 4; i++) {
       var result = response.playlists;
       var playlistURL = result.items[i].external_urls.spotify;
 
-      var imgURL = result.items[i].images[0].url;      
+      var imgURL = result.items[i].images[0].url;
 
       var playlists = $("<div id = 'playlist'>");
       var playlist = $("<a href='" + playlistURL + "' target = 'blank'>");
       var img = $("<img>");
-      img.attr("src"
-        , imgURL);
-      img.addClass("uk-animation-scale-up uk-transform-origin-top-left uk-transition-fade");
-      img.attr("background-color", "black")
+      img.attr("src", imgURL);
+      img.addClass(
+        "uk-animation-scale-up uk-transform-origin-top-left uk-transition-fade"
+      );
+      img.attr("background-color", "black");
 
-      var playDiv =  $("<div id='play'>").text("►");
+      var playDiv = $("<div id='play'>").text("►");
       playDiv.addClass("uk-transition-fade");
-      
 
       playlist.addClass("uk-transition-toggle");
       playlist.addClass("uk-overflow-hidden");
       playlist.append(img);
       playlist.append(playDiv);
 
-
       playlists.append(playlist);
-      
+
       $("#playlistDiv").append(playlists);
     }
-  })
+  });
 }
 
+//firebase
+var config = {
+  apiKey: "AIzaSyC4Oa03PsGzQVoElWaQhppeLBDxINlYfYk",
+  authDomain: "pinme-270ea.firebaseapp.com",
+  databaseURL: "https://pinme-270ea.firebaseio.com",
+  projectId: "pinme-270ea",
+  storageBucket: "pinme-270ea.appspot.com",
+  messagingSenderId: "806008211699",
+  appId: "1:806008211699:web:f94ec750052f45f3"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+googleSignIn = () => {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+      console.log(result);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+};
